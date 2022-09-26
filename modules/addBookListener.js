b07542261aa3cdbda6cbd4b8e4757c213d221341
devMunyi <samunyi90@gmail.com>
2022-09-26 20:12:50 +0300

    fix linters error

--- modules/addBookListener.js
+++ modules/addBookListener.js
@@ -8,7 +8,7 @@ const addBookListener = () => {
     const bookObject = new Awesomebooks(
       bookId,
       document.getElementById('title').value,
-      document.getElementById('author').value
+      document.getElementById('author').value,
     );
     Awesomebooks.addBook(bookObject);
     document.getElementById('title').value = '';
